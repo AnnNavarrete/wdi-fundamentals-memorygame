f5632a68faba20b21ea09808c9d148e608ff2937
AnnNavarrete <ann.g.navarrete@gmail.com>
2019-03-04 13:02:41 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 //console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+var length = cards.length;
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2)
+{
+	console.log("two cards have been played")
+	if(cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match!");
+	else
+		alert("Sorry, try again.");
+}
+
+
