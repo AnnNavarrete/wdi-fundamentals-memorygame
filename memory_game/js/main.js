3820b01503425f3ae833e283a1313e4c0c89db63
AnnNavarrete <ann.g.navarrete@gmail.com>
2019-03-05 18:25:23 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,69 +1,79 @@
 //console.log("Up and running!");
 
-var cards = [
-// Storing an object for each card 
-{
-	rank: "queen",
-	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png",
-},
-{
-	rank: "queen",
-	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png",
-},
-{
-	rank: "king",
-	suit: "hearts",
-	cardImage: "images/king-of-hearts.png",
-},
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png",
-},
+// Storing an object for each card in an array
+var cards = 
+	[{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	},];
 
-];
 
 var cardsInPlay = [];
 
+// Check for a match function and logging into console
 var checkForMatch = function ()
-{
-	if (cardsInPlay[0] === cardsInPlay[1]) 
+	{	
+		if (cardsInPlay.length === 2) 
+		{
+			if (cardsInPlay[0] === cardsInPlay[1]) 
+			{
+				console.log("You found a match!");
+				alert("You found a match!");
+			}	 
+			else 
+			{
+				console.log("Sorry, try again.");
+				alert("Sorry, try again.");
+			}
+		}
+	};
+
+// Adding a flipcard function and clean up code to remove repetition
+var flipCard = function ()
 	{
-	console.log("You found a match!");
-	}	 
-	else 
+		var cardID = this.getAttribute('data-id');
+		this.setAttribute('src', cards[cardID].cardImage);
+		var length = cards.length;
+
+		// Accessing the rank property of this object
+		console.log("User flipped " + cards[cardID].rank);
+		cardsInPlay.push(cards[cardID].rank); 
+
+		// Test the cards flipped in console
+		console.log(cards[cardID].cardImage);
+		console.log(cards[cardID].suit);
+
+		checkForMatch();
+
+	};
+
+// Creating the board
+var createBoard = function ()
 	{
-	console.log("Sorry, try again.");
-	}
-}
+		for (var i = 0; i < cards.length; i++)
+		{
+			var cardElement = document.createElement('img');
+			cardElement.setAttribute('src', 'images/back.png');
+			cardElement.setAttribute('data-id', i);
+			cardElement.addEventListener('click', flipCard);
+			document.getElementById('game-board').appendChild(cardElement);
+		}
+	};
 
-var flipCard = function (cardID)
-{
-	var length = cards.length;
-
-	// Accessing the rank property of this object
-	console.log("User flipped " + cards[cardID].rank);
-	cardsInPlay.push(cards[cardID].rank); 
-
-	// Test the cards flipped in console
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
-
-
-	if (checkForMatch)
-	{
-		if(cardsInPlay[0] === cardsInPlay[1])
-			alert("You found a match!");
-		else
-			alert("Sorry, try again.");
-	}
-
-};
-
-flipCard(0);
-flipCard(2);
-
-
-
+createBoard();
